366fbe9eb293d29ea5eb47409b43c0d2740a403b
WESLEY DE SOUZA DA SILVA <wesley.ssilva48@senacsp.edu.br>
2025-03-06 22:02:41 -0300

    node

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ console.log("Electron processo principal")
 // Importação dos recursos do framework
 // app se refere a aplicação 
 // BrowserWindow (criação da janela)
-const { app, BrowserWindow } = require('electron/main')
+// nativeTheme está relacionado ao tema claro ou escuro (definir) 
+// Menu (definir um menu personalizado )
+// shell (acessar links externos no navegador padrão)
+const { app, BrowserWindow, nativeTheme, Menu, shell } = require('electron/main')
 
 // Janela principal
 let win
 const createWindow = () => {
-win = new BrowserWindow({
+  // definindo tema da janela claro ou escuro
+  nativeTheme.themeSource = 'light'
+  win = new BrowserWindow({
     width: 1010, // Largura
      height: 720, // Altura
     //frame: false
@@ -18,8 +23,33 @@ win = new BrowserWindow({
     // autoHideMenuBar: true // fesconder o menu
   })
 
+  // Carregar o menu personalizado
+  // Atenção!!!! Antes importar o recurso menu
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+
   // carregar o documneto na janela
   win.loadFile('./src/views/index.html')
+}
+
+//janela sobre
+function aboutWindow() {
+  nativeTheme.themeSource='light'
+  // obter a janela principal 
+  const mainWindow = BrowserWindow.getFocusedWindow()
+  // validação (se existir a janela pricipal)
+  if (mainWindow) {
+    about = new BrowserWindow({
+      width: 320,
+      height: 280,
+      autoHideMenuBar: true,
+      resizable: false,
+      minimizable: false,
+      parent: mainWindow,  // estabelecer uma relação hierárquica entre janelas 
+      modal: true
+    })
+  }
+  
+  about.loadFile('./src/views/sobre.html')
 }
 
 // inicialização da aplicação 
@@ -40,3 +70,65 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+// reduzir a verbosidade de logs não criticos  (divitools)
+app.commandLine.appendSwitch('log-level','3')
+
+// template do menu  
+const template = [
+  {
+    label: 'Notas',
+    submenu: [
+      {
+        label: 'Criar nota',
+        accelerator: 'Ctrl+N',
+      },
+      {
+         type: 'separator'
+      },
+      {
+        label: 'Sair',
+        accelerator: 'Alt+F4',
+        click: () => app.quit()
+      }
+    ]
+  },
+  {
+    label: 'Ferramentas',
+    submenu: [
+      {
+        label: 'Aplicar zoom',
+        role: 'zoomIn'
+      },
+      {
+        label:'Reduzir zoom',
+        role: 'zoomOut'
+      },
+      {
+        label: 'Restaurar o zoom padrão',
+        role: 'resetZoom'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'DevTools',
+        role: 'toggleDevTools'
+      }
+    ]
+  },
+  {
+    label: 'Ajuda',
+    submenu: [
+      {
+        label: 'Repositório',
+        click: () => shell.openExternal('https://github.com/WesleySouzaSilva97')
+      },
+      {
+        label: 'Sobre',
+        click: () => aboutWindow()
+      }
+      
+    ]
+  }
+]
